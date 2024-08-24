05ffaddf48c0e2693e06f1728f87248607d4c783
ishikanimmagadda <ishikan84@gmail.com>
2024-08-23 21:14:41 -0700

    p2done

--- hw7.py
+++ hw7.py
@@ -10,6 +10,22 @@ def collatz(n):
         seq = [n] + collatz(new)
     return seq 
 
-print(collatz(5))  
-print(collatz(1))  
-print(collatz(123))  
+# print(collatz(5))  
+# print(collatz(1))  
+# print(collatz(123))  
+
+def find_min(num_list): 
+    if len(num_list) == 1: 
+        min = num_list[0]
+        return min
+    min = num_list[0]
+    if find_min(num_list[1:]) > min: 
+        return min
+    else: 
+        min  = find_min(num_list[1:])
+    return min
+ 
+print(find_min([8]))  
+print(find_min([0, 2, -5, -2, 5, -1, 4, 0, -5, -1]))
+print(find_min([30, 40, 20, 34, 32, 34, 48, 43, 21]))  
+        
