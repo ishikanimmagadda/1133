fa89717258bd11a9ac82628c6adf2c5a72bd8bb8
ishikanimmagadda <ishikan84@gmail.com>
2024-08-25 21:34:05 -0700

    if name exists, overwriting value in a csv file

--- hw8/hw8.py
+++ hw8/hw8.py
@@ -1,6 +1,3 @@
-## Your Name
-## Your x500
-
 #Part 1: get_data_list
 #==========================================
 # Purpose:
@@ -38,15 +35,13 @@ def get_data_list(fname):
 #   OR returns -1 if there is no column labelled 'hw8 Grade'
 #==========================================
 def hw8_index(row1_str):    
-    
     try: 
         row_list = row1_str.split(",")
         hw8_index = row_list.index('hw8 Grade')
         return hw8_index
     except: 
         return -1  
-    #Hint: You may use list.index(), but must prevent an error if the
-    #'hw8 Grade' column is not present
+    
 # print(hw8_index("hw1 Grade,hw2 Grade,hw3 Grade,hw4 Grade,hw5 Grade,hw6 Grade,hw7 Grade,hw8 Grade,hw9 Grade\n"))
 # print(hw8_index("HW8 grade,HW8 Graid,hw8 Grade,HW 8grade,Homework8 grade\n"))
 # print(hw8_index('This,file,exists,,\n'))
@@ -69,7 +64,6 @@ def alter_grade(row_str,idx):
     row_list[idx] = '40'
     altered_str = ",".join(row_list)
     return altered_str
-    #Hint: Use .split and .join
 
 print(alter_grade("30,20,0,0,\n",2))
 print(alter_grade("5,2,5,6,0,0,minutes,\n",1))
@@ -87,15 +81,27 @@ print(alter_grade("Bulbasaur,Charmander,Squirtle,Weedle,Pidgey\n",3))
 #   Otherwise, returns True
 #==========================================
 def haxx(fname):
-
-    #Hints:
-    #   Use get_data_list to read in the rows from the file
-    #   Use hw8_index to determine which column you need to change
-    #   Write back each row unchanged, unless it contains your
-    #   full name, exactly as it appears on Canvas
-    #   If it does contain your name, use alter_grade to create an
-    #   altered row string to write to the file instead
-    #   Be sure to close the file
-
-    return
+    try: 
+        data_list = get_data_list(fname)
+        column_index = hw8_index(data_list[0])
+        if column_index == -1: 
+            return False 
+        f = open(fname, 'w')
+        for row in data_list: 
+            if "Ishika Nimmagadda" in row: 
+                row_str_altered = alter_grade(row, column_index)
+                f.write(row_str_altered)
+            else: 
+                f.write(row)
+        
+        f.close()    
+        return True 
+    except: 
+        return False 
+    
+print(haxx('fakefile.notreal'))
+print(haxx('smallfile.csv'))
+print(haxx('grades1.csv'))
+print(haxx('grades2.csv'))
+print(haxx('grades3.csv'))
             
