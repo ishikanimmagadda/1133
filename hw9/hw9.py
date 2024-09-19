1c2b8393f629d545de72b1f3ebb843c5c8fcb1d0
ishikanimmagadda <ishikan84@gmail.com>
2024-09-18 19:51:36 -0700

    hw9 p1funtion2done

--- hw9/hw9.py
+++ hw9/hw9.py
@@ -10,10 +10,35 @@ def first_words(fname):
 # print(first_words('short1.txt'))
 # print(first_words('short2.txt'))
 
-def next_words(fname): 
-    file1 = open(fname, 'r')
-    dic = {}
-    for word in file1.split(" "): 
+def next_words(fname):
+    final_dic = {}
+    
+    with open(fname, 'r') as file1:
+        # Reading the whole content and splitting by space
+        words = file1.read().split()
+        
+        for i in range(len(words)):
+            current_word = words[i]
+            
+            # If the current word is not the last word
+            if i < len(words) - 1:
+                next_word = words[i + 1]
+            else:
+                next_word = "."
+            
+            # If the word is already in the dictionary, append to the list
+            if current_word in final_dic:
+                final_dic[current_word].append(next_word)
+            else:
+                final_dic[current_word] = [next_word]
+    
+    return final_dic
+
+             
+            
+print(next_words('short1.txt'))
+#print(next_words('short2.txt'))
+    
         
         
     
