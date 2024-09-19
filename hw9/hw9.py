842830a8d815f527c607f0c7dd124d9ddaa6976b
ishikanimmagadda <ishikan84@gmail.com>
2024-09-19 16:24:44 -0700

    part1done!

--- hw9/hw9.py
+++ hw9/hw9.py
@@ -37,8 +37,28 @@ def next_words(fname):
 
              
             
-print(next_words('short1.txt'))
+#print(next_words('short1.txt'))
 #print(next_words('short2.txt'))
+import random
+def fanfic(fname): 
+    file1 = open(fname, 'r')
+    for i in range(0,10): 
+        final = " "
+        first_word = random.choice(first_words(fname))
+        final = final + first_word 
+        dict = next_words(fname)
+        next_word = " "
+        while(next_word != "."):
+            val_list = dict[first_word]
+            next_word = random.choice(val_list)
+            final = final + " " + next_word
+            first_word = next_word
+        print(final)
+        
+print(fanfic('short1.txt'))
+#print(fanfic('short2.txt'))
+
+        
     
         
         
