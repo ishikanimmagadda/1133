84451c9642aef74a2ad4801a6f61e3c7020b4de5
ishikanimmagadda <ishikan84@gmail.com>
2024-11-22 11:15:51 -0800

    hw12 start

---
+++ hw12/hw12.py
@@ -0,0 +1,115 @@
+import tkinter as tk
+import random
+
+#FIRST: Implement and test your Pokemon class below
+class Pokemon:
+    print("Implement this and then remove this print statement")
+
+
+#NEXT: Complete the class definition provided below
+class SafariSimulator(tk.Frame):
+    def __init__(self, master=None):
+        print("In SafariSimulator init")
+        #Read in the data file from pokedex.csv at some point here
+        #It's up to you how you store and handle the data 
+        #(e.g., list, dictionary, etc.),
+        #but you must use your Pokemon class in some capacity
+
+        #Initialize any instance variables you want to keep track of
+
+        #DO NOT MODIFY: These lines set window parameters and create widgets
+        tk.Frame.__init__(self, master)
+        master.minsize(width=275, height=350)
+        master.maxsize(width=275, height=350)
+        master.title("Safari Zone Simulator")
+        self.pack()
+        self.createWidgets()
+
+        #Call nextPokemon() method here to initialize your first random pokemon
+
+    def createWidgets(self):
+        print("In createWidgets")
+        #See the image in the instructions for the general layout required
+        #"Run Away" button has been completed for you as an example:
+        self.runButton = tk.Button(self)
+        self.runButton["text"] = "Run Away"
+        self.runButton["command"] = self.nextPokemon
+        self.runButton.pack()
+        
+        #You need to create an additional "throwButton"
+
+        #A label for status messages has been completed for you as an example:
+        self.messageLabel = tk.Label(bg="grey")
+        self.messageLabel.pack(fill="x", padx=5, pady=5)
+
+        #You need to create two additional labels:
+
+        #Complete and pack the pokemonImageLabel here.
+
+        #Complete and pack the catchProbLabel here.
+
+    def nextPokemon(self):
+        print("In nextPokemon")
+        
+        #This method must:
+            #Choose a random pokemon
+            #Get the info for the appropriate pokemon
+            #Ensure text in messageLabel and catchProbLabel matches the pokemon
+            #Change the pokemonImageLabel to show the right pokemon
+
+        #Hint: to see how to create an image, look at the documentation 
+        #for the PhotoImage/Label classes in tkinter.
+        
+        #Once you generate a PhotoImage object, it can be displayed 
+        #by setting self.pokemonImageLabel["image"] to it
+        
+        #Note: the PhotoImage object MUST be stored as an instance
+        #variable for some object (you can just set it to self.photo).
+        #Not doing this will, for weird memory reasons, cause the image 
+        #to not be displayed.
+        
+    def throwBall(self):
+        print("In throwBall")
+        
+        #This method must:
+
+            #Decrement the number of balls remaining
+            #Try to catch the pokemon
+            #Check to see if endAdventure() should be called
+
+        #To determine whether or not a pokemon is caught, generate a random
+        #number between 0 and 1, using random.random().  If this number is
+        #less than min((catchRate+1), 151) / 449.5, then it is caught. 
+        #catchRate is the integer in the Catch Rate column in pokedex.csv, 
+        #for whatever pokemon is being targetted.
+        
+        #Don't forget to update the throwButton's text to reflect one 
+        #less Safari Ball (even if the pokemon is not caught, it still 
+        #wastes a ball).
+        
+        #If the pokemon is not caught, you must change the messageLabel
+        #text to "Aargh! It escaped!"
+        
+        #Don't forget to call nextPokemon to generate a new pokemon 
+        #if this one is caught.
+        
+    def endAdventure(self):
+        print("In endAdventure")
+        
+        #This method must: 
+
+            #Display adventure completion message
+            #List captured pokemon
+
+        #Hint: to remove a widget from the layout, you can call the 
+        #pack_forget() method.
+        
+        #For example, self.pokemonImageLabel.pack_forget() removes 
+        #the pokemon image.
+
+
+
+
+#DO NOT MODIFY: These lines start your app
+app = SafariSimulator(tk.Tk())
+app.mainloop()
